c823a984507dc3ae70742d2b5f847f7d8b728241
aalberty <46965141+aalberty@users.noreply.github.com>
2023-10-02 15:49:46 +0000

    working async/await convention call

--- scratchpad.js
+++ scratchpad.js
@@ -1,39 +1,69 @@
 const axios = require('axios');
+var activeSession = "";
 
-const getPlayToWinEvents = async (sessionId) => {
+const setSessionId = (sessionId) => {
+    activeSession = sessionId;
+}
+
+const getPlayToWinEvents = async (page) => {
     const response = await axios({
         method: "GET",
         url: "https://tabletop.events/api/playtowin",
         headers: {
-            "session_id": sessionId
+            "session_id": activeSession
         }
     });
     return response;
 }
 
-const getConventions = async (sessionId, page) => {
-    page = page || false;
-    //TODO: add page param to call
-    const response = await axios({
-        method: "GET",
-        url: "https://tabletop.events/api/convention",
-        headers: {
-            session_id: sessionId
-        }
-    });
-    return response;
-}
 
-const conventionCallback = (response, data) => {
-    if (response.data.result.paging) {
-        var pagination = response.data.result.paging;
-        if (pagination.page_number == pagination.total_pages) {
-            return data.append(response.data.result.items);
+/** START CONVENTION BLOCK */
+
+const getConventions = async () => {
+    let conventions = [];
+    let keepGoing = true;
+    let currentPage = 1;
+
+    while (keepGoing) {
+        let response = await reqConventions(currentPage);
+        conventions = conventions.concat.apply(conventions, response.items);
+
+        if (response.paging && (response.paging.page_number != response.paging.total_pages)) {
+            currentPage = response.paging.next_page_number;
         }
-        
-        return conventionCallback();//TODO: next page call
+
+        else {
+            keepGoing = false;
+            return conventions;
+        }
     }
+    
 }
+
+const reqConventions = async (page) => {
+    var formData = new FormData();
+    formData.append("_page_number", page);
+    formData.append("_items_per_page", 100);
+
+    var res = [];
+    try {
+        res = await axios({
+            method: "GET",
+            url: "https://tabletop.events/api/convention",
+            headers: {
+                session_id: activeSession,
+                "Content-Type": "multipart/form-data"
+            },
+            data: formData
+        });
+    } catch (e) {
+        console.warn("An exception occurred with the API request: ", e);
+    }
+
+    return res.data.result;
+}
+
+/** END CONVENTION BLOCK */
 
 const openSession = (username, password, apiKey) => {
     let data = '';
@@ -52,10 +82,10 @@ const openSession = (username, password, apiKey) => {
         });
 }
 
-const closeSession = (sessionId, creds) => {
+const closeSession = (creds) => {
     axios({
         method: "DELETE",
-        url: "https://tabletop.events/api/session/" + sessionId,
+        url: "https://tabletop.events/api/session/" + activeSession,
         data: creds
     })
         .then(function (response) {
