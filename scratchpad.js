b58b34d51af549ec6094ae1bc0d5adaec21a3887
aalberty <46965141+aalberty@users.noreply.github.com>
2023-10-03 20:05:51 +0000

    Added libs and playtowin get/req function stubs

--- scratchpad.js
+++ scratchpad.js
@@ -65,6 +65,107 @@ const reqConventions = async (page) => {
 
 /** END CONVENTION BLOCK */
 
+
+/** START PLAYTOWIN BLOCK */
+const getPtw = async () => {
+    let ptw = [];
+    let keepGoing = true;
+    let currentPage = 1;
+
+    while (keepGoing) {
+        let response = await reqPtw(currentPage);
+        ptw = ptw.concat.apply(ptw, response.items);
+
+        if (response.paging && (response.paging.page_number != response.paging.total_pages)) {
+            currentPage = response.paging.next_page_number;
+        }
+
+        else {
+            keepGoing = false;
+            return ptw;
+        }
+    }
+    
+}
+
+const reqPtw = async (page) => {
+    var formData = new FormData();
+    formData.append("_page_number", page);
+    formData.append("_items_per_page", 100);
+
+    var res = [];
+    try {
+        res = await axios({
+            method: "GET",
+            url: "https://tabletop.events/api/playtowin",
+            headers: {
+                session_id: activeSession,
+                "Content-Type": "multipart/form-data"
+            },
+            data: formData
+        });
+    } catch (e) {
+        console.warn("An exception occurred with the API request: ", e);
+    }
+
+    return res.data.result;
+}
+
+
+/** END PLAYTOWIN BLOCK */
+
+
+/** START LIBRARY BLOCK */
+const getLibraries = async () => {
+    let libs = [];
+    let keepGoing = true;
+    let currentPage = 1;
+
+    while (keepGoing) {
+        let response = await reqLibraries(currentPage);
+        libs = libs.concat.apply(libs, response.items);
+
+        if (response.paging && (response.paging.page_number != response.paging.total_pages)) {
+            currentPage = response.paging.next_page_number;
+        }
+
+        else {
+            keepGoing = false;
+            return libs;
+        }
+    }
+    
+}
+
+const reqLibraries = async (page) => {
+    var formData = new FormData();
+    formData.append("_page_number", page);
+    formData.append("_items_per_page", 100);
+
+    var res = [];
+    try {
+        res = await axios({
+            method: "GET",
+            url: "https://tabletop.events/api/library",
+            headers: {
+                session_id: activeSession,
+                "Content-Type": "multipart/form-data"
+            },
+            data: formData
+        });
+    } catch (e) {
+        console.warn("An exception occurred with the API request: ", e);
+    }
+
+    return res.data.result;
+}
+
+//TODO: Retrieve a specific library with _include_relationships=1 added to form data; this should
+//include playtowin items related to the library. See if this has historical, or only present data associated.
+
+/** END LIBRARY BLOCK */
+
+//openSession(process.env.TTE_USERNAME, process.env.TTE_PWD, process.env.TTE_API_KEY)
 const openSession = (username, password, apiKey) => {
     let data = '';
     
