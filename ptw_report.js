689b6a5d09c6a0970bbb14ccc6db1fefdd8c0faa
aalberty <46965141+aalberty@users.noreply.github.com>
2023-10-16 20:47:26 +0000

    mvp playtowin report

--- ptw_report.js
+++ ptw_report.js
@@ -15,19 +15,33 @@ const getGCPtwData = () => {
     var endpoint = "/library/6CF7A168-92DD-11E8-A295-3F59BA594530/playtowins";
     
     var response = api.get(endpoint, params);
-    var data = [];
+    var report = [];
     response.then((res)=>{
         res.forEach(element => {
-            data.push(element);
+            try{
+                report.push({
+                    name: element.name,
+                    badge_number: element.badge && element.badge.badge_number ? element.badge.badge_number : "unknown",
+                    winner: element.win == 1 ? true : false,
+                    game: element.librarygame.name,
+                    convention: element.convention.name
+                });
+            } catch (e) {
+                console.warn("Error adding playtowin entry to report: ", element);
+            }
         });
 
         //TODO: filter to pertinent PTW results (specific year? all? etc)
         //TODO: for each pertinent PTW entry, go find out checkout events from this year 30min or greater.
         // if no checkouts 30 min or greater are found, this winner is inelligable. (For Brian - how do we determine)
         // the new winner in this case? Select new random winner?
-
-        console.log(data[0]);
-
     });
+    
+    return report;
+}
 
+const filterWinners = (data, fullYear) => {
+    return data.filter((entry)=>{
+        return entry.winner && entry.convention.indexOf(fullYear) != -1;
+    });
 }
